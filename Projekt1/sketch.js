c3b78f252b809dc492a877f2cb78786053b93c89
LinJLK <60345263+LinJLK@users.noreply.github.com>
2020-08-19 12:28:03 +0200

    Add files via upload

--- Projekt1/sketch.js
+++ Projekt1/sketch.js
@@ -1,9 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  // put setup code here
+  createCanvas(800, 600);
+  background(240);
 }
 
 function draw() {
-  background(220);
-  ellipse(200,200,200,200);
-  
+  // put drawing code here
+point(10,10);
+point(790,590);
+point(10,590);
+point(790,10);
+line(10,10,790,590);
+
+if(mouseIsPressed)
+{
+	fill(244,mouseY,0);
+}
+else{
+	fill(255);
+}
+noStroke();
+ellipse(mouseX,mouseY,80,80);
+
 }
